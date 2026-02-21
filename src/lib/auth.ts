b41248a93b2da92c72314bf981c3392483d1c843
Milind Mishra <milind.mishra4@gmail.com>
2026-02-21 15:40:56 +0530

    feat: update scope

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,6 +17,7 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      scope: ['read:user', 'user:email', 'repo'],
     },
   },
 
