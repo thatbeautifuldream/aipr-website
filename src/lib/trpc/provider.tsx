d423574a8ee3c0ac6d3651eeb987a4a688913f6a
shubhankiit <shubhankit@foyer.work>
2026-02-21 15:11:36 +0530

    fix: revert utils

--- src/lib/trpc/provider.tsx
+++ src/lib/trpc/provider.tsx
@@ -4,12 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { httpBatchLink } from '@trpc/react-query'
 import React, { useState } from 'react'
 import SuperJSON from 'superjson'
+import { getBaseUrl } from '../utils'
 import { trpc } from './client'
-
-export function getBaseUrl() {
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
-  return 'http://localhost:3000'
-}
 
 export function TRPCProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
