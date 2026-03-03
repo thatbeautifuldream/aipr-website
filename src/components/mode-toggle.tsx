5961c50075d75c2f6d6ea7aab8a803909bcc3178
Milind Mishra <milind.mishra4@gmail.com>
2026-03-04 03:22:25 +0530

    feat: update the non-authenticated state mode toggle button to use just copy instead of icon

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { MoonIcon } from '@/components/icons/moon-icon'
-import { SunIcon } from '@/components/icons/sun-icon'
 import { useTheme } from 'next-themes'
 import * as React from 'react'
 
@@ -21,10 +19,9 @@ export function ModeToggle() {
     <button
       type="button"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-      className="relative inline-flex h-9 w-9 items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
+      className="inline-flex h-9 cursor-pointer items-center justify-center rounded-md px-3 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
     >
-      <SunIcon className="absolute h-5 w-5 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <MoonIcon className="absolute h-5 w-5 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      {theme === 'light' ? 'Light' : 'Dark'}
       <span className="sr-only">Toggle theme</span>
     </button>
   )
